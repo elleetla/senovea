d881dc8407a69a313099ba6d345a8764ec808376
BRAUPERLE <lilian.tourillon@gmail.com>
2018-08-17 12:10:42 +0200

    m

--- config/config-api.js
+++ config/config-api.js
@@ -1,3 +1,3 @@
 export const WORDPRESS_API_BASE_URL = process.env.NODE_ENV === 'production' ?
         'https://senovea.juliengrelet.com/wp-json' :
-        'https://senovea.juliengrelet.com/wp-json';
+        'http://senovea-wp.brauperle/wp-json';
