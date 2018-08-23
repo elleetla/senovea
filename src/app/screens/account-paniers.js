acaedbb47d1c966a47b679b0aac3568205ce3a39
elleetla <j.grelet@elle-et-la.com>
2018-08-23 11:18:31 +0200

    add

--- src/app/screens/account-paniers.js
+++ src/app/screens/account-paniers.js
@@ -17,9 +17,7 @@ class AccountPaniers extends Component{
 
     
     renderNotSentPanier( panier ){
-        
-        //console.log( panier );
-        
+        console.log("Panierrrrr à valider : ", panier);
         return(
             <div key={panier.id} className="bloc-panier">
                 <Row style={{marginBottom:"15px"}}>
@@ -30,10 +28,7 @@ class AccountPaniers extends Component{
                                 {
                                     this.props.paniers !== 0 ?
                                     <span>
-                                        Panier du : 
-                                        <strong>
-                                            {moment(panier.panier_date_created).locale('fr').format('L')}
-                                        </strong>
+                                        Panier du : <strong>{moment(panier.panier_date_created).locale('fr').format('L')}</strong>
                                     </span>
                                     : 
                                     null
@@ -48,7 +43,7 @@ class AccountPaniers extends Component{
                 <Row>
                     <Col md={6}>
                             <div>
-                                <strong>XX</strong> Articles
+                                <strong>{panier.quantity}</strong> Articles
                             </div>
                             <div>
                                 Montant HT <strong>XXX</strong> $
@@ -64,11 +59,10 @@ class AccountPaniers extends Component{
                 </Row>
             </div>
         )
-        
     }
     
     renderSentPanier( panier ){
-
+         console.log("Panierrrrr en cours : ", panier);
         const groupedArticlesByFournisseurs = _.groupBy( panier.products_lots , ( product ) => {
             return product.lot.lot_fournisseur_r1.ID;
         } );
@@ -77,37 +71,26 @@ class AccountPaniers extends Component{
 
         return(
             <div key={panier.id} className="bloc-panier bloc-panier-sent">
-                <Row style={{marginBottom:"15px"}}>
-                    <Col md={6}>
+                <Row style={{marginBottom:"30px"}}>
+                    <Col md={6} className="d-flex align-items-center">
                         <div style={{display:"flex",alignItems:"center"}}>
                             <div className="statut-panier">En Cours</div>
-                            <div>
-                                {
-                                    this.props.paniers !== 0 ?
-                                    <span>
-                                        Panier du : 
-                                        <strong>
-                                            {moment(panier.panier_date_created).locale('fr').format('L')}
-                                        </strong>
-                                    </span>
-                                    : 
-                                    null
-                                }
-                            </div>
+                            { this.props.paniers !== 0 ? <span className="date-panier">Panier du : <b>{moment(panier.panier_date_created).locale('fr').format('L')}</b></span> : null }
                         </div>
                     </Col>
-                    <Col md={6}>
-                        <h3 style={{fontSize: "22px", textAlign: "right"}}>{panier.nicename}</h3>
+                    <Col md={6} className="d-flex align-items-center">
+                        <h3 className="title-panier">{panier.nicename}</h3>
                     </Col>
                 </Row>
-                <Row style={{marginBottom:"15px"}}>
-                    <Col md={12}>
-                        <div>
-                            <strong>XX</strong> Articles
-                        </div>
-                        <div>
-                            Montant HT <strong>XXX</strong> $
-                        </div>
+                <Row>
+                    <Col md={8} className="d-flex align-items-center">
+                        <ul className="list-infos-panier">
+                            <li><b>6</b> Articles</li>
+                            <li>Montant Total HT <b>7000</b> €</li>
+                        </ul>
+                    </Col>
+                    <Col md={4}>
+                         <button className="btn-green float-right">+ d'infos</button>
                     </Col>
                 </Row>
                 <Row>
@@ -219,10 +202,6 @@ class AccountPaniers extends Component{
     }
     
     render(){
-        
-        //console.log( "compte paniers" )
-        //console.log( this.props )
-        
         return(
             <Account>   
             <Container>
